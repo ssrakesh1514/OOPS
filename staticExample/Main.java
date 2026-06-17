bb32f4a218f539297db04372f66500a4d833b0fa
Rakesh S S <115566786+ssrakesh1514@users.noreply.github.com>
2026-06-17 14:25:27 +0530

    Update Main.java

--- staticExample/Main.java
+++ staticExample/Main.java
@@ -1,19 +1,40 @@
-package com.kunal.singleton;
-
-import com.kunal.access.A;
+package com.kunal.staticExample;
 
 public class Main {
     public static void main(String[] args) {
-        Singleton obj1 = Singleton.getInstance();
+//        Human kunal = new Human(22, "Kunal", 10000, false);
+//        Human rahul = new Human(34, "Rahul", 15000, true);
+//        Human arpit = new Human(34, "arpit", 15000, true);
+//
+//        System.out.println(Human.population);
+//        System.out.println(Human.population);
+//        System.out.println(Human.population);
 
-        Singleton obj2 = Singleton.getInstance();
+        Main funn = new Main();
+        funn.fun2();
 
-        Singleton obj3 = Singleton.getInstance();
+    }
 
-        // all 3 ref variables are pointing to just one object
+    // this is not dependent on objects
+    static void fun() {
+//        greeting(); // you cant use this because it requires an instance
+        // but the function you are using it in does not depend on instances
 
-        A a = new A(10, "Kunal");
-        a.getNum();
-//        int n = a.num;
+        // you cannot access non static stuff without referencing their instances in
+        // a static context
+
+        // hence, here I am referencing it
+        Main obj = new Main();
+        obj.greeting();
+    }
+
+    void fun2() {
+        greeting();
+    }
+
+    // we know that something which is not static, belongs to an object
+    void greeting() {
+//        fun();
+        System.out.println("Hello world");
     }
 }
